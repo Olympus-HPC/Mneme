b4d3b6544c6e9c322e98fcc3642d78ef92d4d1a8
koparasy <parasyris1@llnl.gov>
2025-01-31 13:02:18 -0800

    Fixed file logger naming

--- src/MnemeLogger.hpp
+++ src/MnemeLogger.hpp
@@ -34,7 +34,7 @@ inline std::string getLogFileName() {
   } else {
     id = std::to_string(getpid());
   }
-  id = "mneme-" + id + ".log";
+  id = "mneme-" + std::string(hostname) + "-" + id + ".log";
   return id;
 }
 
@@ -73,11 +73,12 @@ inline void initialize() {
   if (!logDir.empty()) {
     std::string logFile = logDir + "/" + getLogFileName();
     logger = spdlog::basic_logger_mt("file_logger", logFile);
+    logger->set_pattern("[mneme] [%^%l%$] %v");
   } else {
     logger = spdlog::stdout_color_mt("console_logger");
+    logger->set_pattern("[\033[34mmneme\033[0m] [%^%l%$] %v");
   }
   logger->set_level(getLogLevelFromEnv());
-  logger->set_pattern("[\033[34mmneme\033[0m] [%^%l%$] %v");
 }
 } // namespace MnemeLogger
 } // namespace mneme
