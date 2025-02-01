51040b0372c0e79003f11e6504acf1dcec16f290
koparasy <parasyris1@llnl.gov>
2025-01-31 16:01:44 -0800

    Adding extra features

--- tests/record/read-record.py
+++ tests/record/read-record.py
@@ -5,4 +5,31 @@ from pathlib import Path
 
 
 for fn in glob.glob("./*.json"):
-    print(fn)
+    with open(fn, "r") as fd:
+        rr_data = json.load(fd)
+    print("DemangledName:", rr_data["DemangledName"])
+    print("NumModules:", rr_data["NumModules"])
+    print("NumInstances:", len(rr_data["instances"]))
+    for instance in rr_data["instances"]:
+        print(
+            "BlockDims:({}, {}, {})",
+            instance["BlockDims"]["x"],
+            instance["BlockDims"]["y"],
+            instance["BlockDims"]["z"],
+        )
+        print(
+            "GridDims:({}, {}, {})",
+            instance["GridDims"]["x"],
+            instance["GridDims"]["y"],
+            instance["GridDims"]["z"],
+        )
+        if (!Path(instance["prologue"]).exists()):
+            print("Expected prologue file to exist")
+            return -1
+        if (!Path(instance["epilogue"]).exists()):
+            print("Expected epilogue file to exist")
+            return -1
+
+    print(rr_data)
+
+sys.exit(-1)
