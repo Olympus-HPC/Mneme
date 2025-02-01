db5f9b869e93958c36c6038433bd2a3800a358d6
koparasy <parasyris1@llnl.gov>
2025-01-31 16:16:38 -0800

    Nit tests

--- tests/record/kernel.cpp
+++ tests/record/kernel.cpp
@@ -1,6 +1,8 @@
 // clang-format off
+// RUN: rm -rf *.json Recorded*.bv DEviceState*.mneme
 // RUN: LD_PRELOAD=MNEME_PRELOAD_LIB MNEME_LOG_LEVEL=debug MNEME_PAGE_SIZE=%PG ./kernel | FileCheck %s --check-prefixes=CHECK
-// RUN: %RR
+// RUN: %RR | FileCheck %s --check-prefix=CHECK-RR
+// RUN: rm -rf *.json Recorded*.bv DEviceState*.mneme
 // clang-format on
 
 #include <climits>
@@ -13,9 +15,14 @@ using namespace mneme;
 using MnemeDeviceRT = DeviceTraits<DeviceVendors::HIP>;
 #endif
 
+// CHECK-RR:DemangledName: kernel()
+// CHECK-RR:NumModules: 1
+// CHECK-RR:NumInstances: 1
 __global__ void kernel() { printf("Kernel\n"); }
 
 int main() {
+  // CHECK-RR:BlockDims:(1, 1, 1)
+  // CHECK-RR:GridDims:(1, 1, 1)
   kernel<<<1, 1>>>();
   auto EC = MnemeDeviceRT::DeviceErrorCheck(MnemeDeviceRT::DeviceSynchronize());
   if (EC) {
--- tests/record/read-record.py
+++ tests/record/read-record.py
@@ -8,28 +8,29 @@ for fn in glob.glob("./*.json"):
     with open(fn, "r") as fd:
         rr_data = json.load(fd)
     print("DemangledName:", rr_data["DemangledName"])
-    print("NumModules:", rr_data["NumModules"])
+    print("NumModules:", len(rr_data["Modules"]))
     print("NumInstances:", len(rr_data["instances"]))
-    for instance in rr_data["instances"]:
+    for k, instance in rr_data["instances"].items():
         print(
-            "BlockDims:({}, {}, {})",
-            instance["BlockDims"]["x"],
-            instance["BlockDims"]["y"],
-            instance["BlockDims"]["z"],
+            "BlockDims:({0}, {1}, {2})".format(
+                instance["BlockDims"]["x"],
+                instance["BlockDims"]["y"],
+                instance["BlockDims"]["z"],
+            )
         )
         print(
-            "GridDims:({}, {}, {})",
-            instance["GridDims"]["x"],
-            instance["GridDims"]["y"],
-            instance["GridDims"]["z"],
+            "GridDims:({0}, {1}, {2})".format(
+                instance["GridDims"]["x"],
+                instance["GridDims"]["y"],
+                instance["GridDims"]["z"],
+            )
         )
-        if (!Path(instance["prologue"]).exists()):
+        if not Path(instance["Prologue"]).exists():
             print("Expected prologue file to exist")
-            return -1
-        if (!Path(instance["epilogue"]).exists()):
+            sys.exit(-1)
+        if not Path(instance["Epilogue"]).exists():
             print("Expected epilogue file to exist")
-            return -1
+            sys.exit(-1)
 
-    print(rr_data)
 
 sys.exit(-1)
