f74c89580bf3588d771287dd45480ee6df621314
koparasy <parasyris1@llnl.gov>
2025-02-17 21:15:29 -0800

    Use events to measure kernel execution time

--- src/MnemeReplay.cpp
+++ src/MnemeReplay.cpp
@@ -1,4 +1,5 @@
 #include <hip/hip_runtime.h>
+#include <mneme/Utils.hpp>
 
 #ifdef ICMP_NE
 #undef ICMP_NE
@@ -12,6 +13,10 @@
 #include "mneme/MnemeJITProteus.hpp"
 #include "mneme/MnemeLogger.hpp"
 #include "mneme/MnemeReplay.hpp"
+
+#include <llvm/IR/Module.h>
+#include <llvm/Support/FileSystem.h>
+#include <llvm/Support/raw_ostream.h>
 
 using namespace mneme;
 using namespace llvm;
@@ -39,12 +44,12 @@ static cl::opt<std::string>
 
 static cl::opt<char> MnemeMiddleOptLevel(
     "middle-opt-level",
-    cl::desc("The optimization level to use when optimizing IR"), cl::init('3'),
+    cl::desc("The optimization level to use when optimizing IR"), cl::init('1'),
     cl::cat(MnemeCategory));
 
 static cl::opt<unsigned> MnemeBackendOptLevel(
     "backend-opt-level",
-    cl::desc("The optimization level to use when optimizing IR"), cl::init(3),
+    cl::desc("The optimization level to use when optimizing IR"), cl::init(1),
     cl::cat(MnemeCategory));
 
 static cl::alias MnemeShortMiddleOptLevel(
@@ -59,6 +64,18 @@ static cl::opt<int>
     MnemeRepeats("repeats",
                  cl::desc("The number of repeat executions for every kernel"),
                  cl::init(3), cl::cat(MnemeCategory));
+
+void writeIRToFile(const llvm::Module &M, const std::string &Filename) {
+  std::error_code EC;
+  llvm::raw_fd_ostream File(Filename, EC, llvm::sys::fs::OF_Text);
+
+  if (EC) {
+    llvm::errs() << "Error opening file: " << EC.message() << "\n";
+    return;
+  }
+
+  M.print(File, nullptr); // Print the module's IR to the file
+}
 
 int main(int argc, char *argv[]) {
   cl::HideUnrelatedOptions(MnemeCategory);
@@ -78,8 +95,11 @@ int main(int argc, char *argv[]) {
   llvm::LLVMContext Ctx;
   auto Modules = RInstance.loadModules(Ctx);
   auto Mod = proteus::linkModules(Ctx, Modules);
-  proteus::pruneIR(*Mod);
+  writeIRToFile(*Mod, "linked.ll");
+  proteus::pruneIR(*Mod, false);
+  writeIRToFile(*Mod, "pruned.ll");
   pruneMnemeGlobals(*Mod);
+  writeIRToFile(*Mod, "prunedMneme.ll");
   auto ReplayKernelFunc = Mod->getFunction(RInstance.getKernelName());
   // TODO: Internalize is too aggresive as is now. We will need to either write
   // our own, or wait for proteus to expose a more generic interface.
@@ -92,6 +112,8 @@ int main(int argc, char *argv[]) {
            MnemeMiddleOptLevel.getValue(), MnemeBackendOptLevel.getValue());
   proteus::optimizeIR(*Mod, Arch, MnemeMiddleOptLevel.getValue(),
                       MnemeBackendOptLevel.getValue());
+
+  writeIRToFile(*Mod, "test.ll");
 
   auto RecordedGrid = RInstance.getRecordedGrid();
   auto RecordedBlock = RInstance.getRecordedBlock();
@@ -114,21 +136,62 @@ int main(int argc, char *argv[]) {
       VendorModule, RInstance.getKernelName());
 
   bool verify = true;
+  DeviceVendorTraits::DeviceStream_t ReplayStream;
+  DeviceVendorTraits::DeviceEvent_t StartEvent, EndEvent;
+  auto EC = DeviceVendorTraits::DeviceErrorCheck(
+      DeviceVendorTraits::DeviceStreamCreate(&ReplayStream));
+  if (EC)
+    FATAL_ERROR("Error when creating a stream for replay\n" + EC.value());
+
+  EC = DeviceVendorTraits::DeviceErrorCheck(
+      DeviceVendorTraits::deviceEventCreate(&StartEvent));
+
+  if (EC)
+    FATAL_ERROR("Error when creating start event for replay\n" + EC.value());
+
+  EC = DeviceVendorTraits::DeviceErrorCheck(
+      DeviceVendorTraits::deviceEventCreate(&EndEvent));
+
+  if (EC)
+    FATAL_ERROR("Error when creating end event for replay\n" + EC.value());
+
+  std::vector<float> timings;
   for (int i = 0; i < _MnemeRepeats; i++) {
+    float elapsedTime;
     LOG_DEBUG("Run {}/{}", i + 1, _MnemeRepeats);
-    DeviceVendorTraits::DeviceStream_t ReplayStream;
-    auto EC = DeviceVendorTraits::DeviceErrorCheck(
-        DeviceVendorTraits::DeviceStreamCreate(&ReplayStream));
-    if (EC)
-      FATAL_ERROR("Error when creating a stream for replay\n" + EC.value());
 
     auto Args = RInstance.getArgs();
+
+    DeviceVendorTraits::DeviceErrorCheck(
+        DeviceVendorTraits::deviceEventRecord(StartEvent, ReplayStream));
+    if (EC)
+      FATAL_ERROR("Error when recording event " + EC.value());
+
     EC = DeviceVendorTraits::DeviceErrorCheck(
         DeviceVendorTraits::launchKernelFunction(
             Func, RecordedGrid, RecordedBlock, Args.get(),
             RInstance.getSharedMemSize(), ReplayStream));
     if (EC)
       FATAL_ERROR("Error When Launching Kernel: " + EC.value());
+
+    DeviceVendorTraits::DeviceErrorCheck(
+        DeviceVendorTraits::deviceEventRecord(EndEvent, ReplayStream));
+    if (EC)
+      FATAL_ERROR("Error when recording event " + EC.value());
+
+    EC = DeviceVendorTraits::DeviceErrorCheck(
+        DeviceVendorTraits::deviceEventSynchronize(EndEvent));
+    if (EC)
+      FATAL_ERROR("Error when synchronizing event " + EC.value());
+
+    EC = DeviceVendorTraits::DeviceErrorCheck(
+        DeviceVendorTraits::deviceEventElapsedTime(&elapsedTime, StartEvent,
+                                                   EndEvent));
+    if (EC)
+      FATAL_ERROR("Error when recording event " + EC.value());
+
+    std::cout << "Kernel execution time: " << elapsedTime << " ms\n";
+    timings.push_back(elapsedTime);
 
     EC = DeviceVendorTraits::DeviceErrorCheck(
         DeviceVendorTraits::DeviceStreamSynchronize(ReplayStream));
@@ -139,6 +202,33 @@ int main(int argc, char *argv[]) {
     verify &= RInstance.isMemorySame();
     RInstance.reset();
   }
+
+  float sum = std::accumulate(timings.begin(), timings.end(), 0.0);
+  float mean = sum / timings.size();
+  // Compute the standard deviation
+  float sq_sum =
+      std::inner_product(timings.begin(), timings.end(), timings.begin(), 0.0);
+  float std_dev = std::sqrt(sq_sum / timings.size() - mean * mean);
+  std::cout << "Mean execution time (ms): " << mean
+            << " standard Deviation: " << std_dev << "\n";
+
+  EC = DeviceVendorTraits::DeviceErrorCheck(
+      DeviceVendorTraits::deviceStreamDestroy(ReplayStream));
+  if (EC)
+    FATAL_ERROR("Error when destroying stream for replay\n" + EC.value());
+
+  EC = DeviceVendorTraits::DeviceErrorCheck(
+      DeviceVendorTraits::deviceEventDestroy(StartEvent));
+
+  if (EC)
+    FATAL_ERROR("Error when destroying start event for replay\n" + EC.value());
+
+  EC = DeviceVendorTraits::DeviceErrorCheck(
+      DeviceVendorTraits::deviceEventDestroy(EndEvent));
+
+  if (EC)
+    FATAL_ERROR("Error when destroying end event for replay\n" + EC.value());
+
   if (verify)
     std::cout << "Results Match" << "\n";
   else
