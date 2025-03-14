9e9063126e0d06d060e0b5b8ad82a9dc6bee3ab2
koparasy <parasyris1@llnl.gov>
2025-03-14 13:22:04 -0700

    Remove prints

--- python/mneme/llvm/utils.py
+++ python/mneme/llvm/utils.py
@@ -11,12 +11,10 @@ def get_library_name():
     Return the name of the llvm4ml shared library file.
     """
     current_file_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    print("Root file path is ", current_file_path)
     if os.name == "posix":
         if sys.platform == "darwin":
             return os.path.abspath(current_file_path + "/libmneme.dylib")
         else:
-            print(os.getcwd())
             return os.path.abspath(current_file_path + "/libmneme.so")
     else:
         return os.path.abspath(current_file_path + "/libmneme.dll")
