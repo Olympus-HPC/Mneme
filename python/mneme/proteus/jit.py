307486dd6e667b70df1c9a14edad2a264bc4dc00
koparasy <parasyris1@llnl.gov>
2025-03-14 15:04:16 -0700

    Fix skeleton

--- python/mneme/proteus/jit.py
+++ python/mneme/proteus/jit.py
@@ -1,12 +1,11 @@
 import typing
-from ../llvm/module.py import ModuleRef
-from ../llvm/ import ffi as ffi
+from ..llvm.module import ModuleRef
+from ..llvm import ffi as ffi
 
 ffi.lib.ProteusPY_pruneIR.argtypes = [ffi.LLVMModuleRef]
 
-def pruneIR(mod: ModuleRef): 
+
+def pruneIR(mod: ModuleRef):
     if not isinstance(mod, ModuleRef):
         raise TypeError(f"Expecting type of ModuleRef instead got {type(mod)}")
     ffi.lib.ProteusPY_pruneIR(mod)
-
-
---
+++ tests/python/read.py
@@ -0,0 +1,21 @@
+import mneme
+import mneme.llvm as llvm
+import mneme.proteus.jit as jit
+import sys
+
+module_bc = sys.argv[1]
+
+with open(module_bc, "rb") as fd:
+    bitcode = fd.read()
+
+Mod = llvm.module.parse_bitcode(bitcode)
+
+for i, func in enumerate(Mod.functions):
+    print(i, func.name)
+
+print(jit.pruneIR)
+jit.pruneIR(Mod)
+
+print("After")
+for i, func in enumerate(Mod.functions):
+    print(i, func.name)
