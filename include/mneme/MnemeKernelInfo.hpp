c2caeac0dd114fc419ee874aa3793e1df1431169
koparasy <parasyris1@llnl.gov>
2025-07-08 09:49:22 -0700

    Record or HIP works

---
+++ include/mneme/MnemeKernelInfo.hpp
@@ -0,0 +1,87 @@
+#pragma once
+
+#include <llvm/ADT/DenseMap.h>
+#include <llvm/ADT/SmallVector.h>
+#include <llvm/ADT/StringRef.h>
+
+#include "mneme/MnemeDeviceBinary.hpp"
+#include "mneme/MnemeSymbols.hpp"
+
+namespace mneme {
+struct KernelInfo {
+  MnemeDeviceLinkedBin &Exec;
+  const void *HostFun;
+  std::string Name;
+  std::optional<uint64_t> StaticHash;
+  llvm::SmallVector<size_t> KernelArgSizes;
+  llvm::SmallVector<std::string> KernelArgNames;
+  llvm::SmallVector<std::function<double(void *)>> ToDoubleFunc;
+  llvm::SmallVector<bool> KernelSpecializations;
+  llvm::SmallVector<std::unique_ptr<uint8_t[]>> ArgData;
+  KernelInfo(MnemeDeviceLinkedBin &Executable, const void *HostFun, char *Name)
+      : Exec(Executable), HostFun(HostFun), Name(Name),
+        StaticHash(std::nullopt) {};
+  KernelInfo(MnemeDeviceLinkedBin &Executable, std::string &Name)
+      : Exec(Executable), Name(Name), StaticHash(std::nullopt) {};
+
+public:
+  void setArgSizes(llvm::ArrayRef<size_t> ArgSizes) {
+    KernelArgSizes = llvm::SmallVector<size_t>(ArgSizes);
+  }
+
+  void setArgNames(llvm::ArrayRef<std::string> Names) {
+    KernelArgNames = llvm::SmallVector<std::string>(Names);
+  }
+
+  void setSpecializations(llvm::ArrayRef<bool> Specializations) {
+    KernelSpecializations = llvm::SmallVector<bool>(Specializations);
+  }
+
+  void setArgData(const char *&Data, int Index) {
+    if (Index >= KernelArgSizes.size() || Index >= ArgData.size())
+      LOG_FATAL("Setting argument data out of range");
+
+    auto MemSize = KernelArgSizes[Index];
+    ArgData[Index] = std::make_unique<uint8_t[]>(MemSize);
+    std::memcpy(static_cast<void *>(ArgData[Index].get()),
+                static_cast<const void *>(Data), MemSize);
+    Data += KernelArgSizes[Index];
+  }
+
+  void setToDoubleFunc(llvm::ArrayRef<std::function<double(void *)>> convert) {
+    ToDoubleFunc = llvm::SmallVector<std::function<double(void *)>>(convert);
+  }
+
+  int64_t getNumArgs() const { return KernelArgSizes.size(); }
+  MnemeDeviceLinkedBin &getHandle() const { return Exec; }
+  const std::string getName() const { return Name; }
+  const void *getFunHandle() const { return HostFun; }
+  llvm::ArrayRef<size_t> getArgSizes() const { return KernelArgSizes; }
+  llvm::ArrayRef<std::string> getArgNames() const { return KernelArgNames; }
+  llvm::ArrayRef<bool> getArgSpecializations() const {
+    return KernelSpecializations;
+  }
+
+  llvm::stable_hash getStaticHash() {
+    if (StaticHash)
+      return StaticHash.value();
+    return computeStaticHash();
+  }
+
+  llvm::ArrayRef<std::unique_ptr<uint8_t[]>> getArgData() const {
+    return ArgData;
+  }
+
+  llvm::ArrayRef<std::function<double(void *)>> getToDoubleFunc() const {
+    return ToDoubleFunc;
+  }
+
+private:
+  llvm::stable_hash computeStaticHash() {
+    StaticHash = llvm::stable_hash_combine_string(Name);
+    StaticHash =
+        llvm::stable_hash_combine(StaticHash.value(), Exec.getStaticHash());
+    return StaticHash.value();
+  }
+};
+} // namespace mneme
