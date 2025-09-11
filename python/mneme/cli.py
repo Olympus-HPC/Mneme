5d71b4a842fd4f2f6d3a7ff0cb7d60a173038a45
Konstantinos Parasyris <parasyris1@llnl.gov>
2025-09-10 17:03:26 -0700

    Add commands to manage mneme files (#46)

--- python/mneme/cli.py
+++ python/mneme/cli.py
@@ -15,6 +15,7 @@ import sys
 import time
 from typing import Tuple
 
+from mneme.commands import Clean, Copy, Move
 from mneme.device import DeviceModule, dim3, get_device_arch
 from mneme.llvm.module import ModuleRef
 from mneme.page_manager import PageManagerRef
@@ -27,7 +28,7 @@ from mneme.tuner import ReplayTuner
 
 def main():
     parser = argparse.ArgumentParser(prog="mneme", description="Mneme Tool")
-    parent_parser = BaseExecutor.get_base_parser()
+    parent_executor_parser = BaseExecutor.get_base_parser()
 
     subparsers = parser.add_subparsers(
         title="commands",
@@ -37,15 +38,36 @@ def main():
     )
 
     p_exec = subparsers.add_parser(
-        "execute", parents=[parent_parser], help="run a recorded kernel"
+        "execute", parents=[parent_executor_parser], help="Run a recorded kernel"
     )
 
     p_tune = subparsers.add_parser(
-        "tune", parents=[parent_parser], help="tune the pipeline of a recorded kernel"
+        "tune",
+        parents=[parent_executor_parser],
+        help="Tune the pipeline of a recorded kernel",
+    )
+
+    p_clean = subparsers.add_parser(
+        "Clean", parents=[], help="Clean mneme generated files"
+    )
+
+    p_copy = subparsers.add_parser(
+        "copy",
+        parents=[],
+        help="Copy mneme generated files and update database accordingly",
+    )
+
+    p_move = subparsers.add_parser(
+        "move",
+        parents=[],
+        help="Move mneme generated files and update database accordingly",
     )
 
     CLIExecutor.set_cli_args(p_exec)
     ReplayTuner.set_cli_args(p_tune)
+    Clean.set_cli_args(p_clean)
+    Copy.set_cli_args(p_copy)
+    Move.set_cli_args(p_move)
 
     args = parser.parse_args()
     return args.func(args)
---
+++ python/mneme/commands.py
@@ -0,0 +1,136 @@
+import argparse
+import os
+import shutil
+import sys
+import time
+from pathlib import Path
+
+from mneme.recorded_execution import RecordedExecution
+
+
+def _copy_or_move(sources, dest, move=False):
+    for s in sources:
+        if not s.exists():
+            continue
+
+        exec = RecordedExecution.from_json(str(s))
+        new_ir_files = []
+        for ll in exec.llvm_files:
+            dest_ll = dest / Path(ll).name
+            if move:
+                shutil.move(ll, dest_ll)
+            else:
+                shutil.copy(ll, dest_ll)
+            new_ir_files.append(str(dest_ll))
+
+        exec.llvm_files = new_ir_files
+
+        for _, kernel in exec.items():
+            kfn = Path(kernel.prologue.fn)
+            dest_kfn = dest / kfn.name
+            if move:
+                shutil.move(kfn, dest_kfn)
+            else:
+                shutil.copy(kfn, dest_kfn)
+            kernel.prologue.fn = str(dest_kfn)
+
+            kfn = Path(kernel.epilogue.fn)
+            dest_kfn = dest / kfn.name
+            if move:
+                shutil.move(kfn, dest_kfn)
+            else:
+                shutil.copy(kfn, dest_kfn)
+            kernel.epilogue.fn = str(dest_kfn)
+
+        json_fn = dest / s.name
+        exec.to_json(str(json_fn))
+        if move:
+            s.unlink()
+
+
+class Clean:
+    @staticmethod
+    def set_cli_args(parser):
+        parser.add_argument("dbs", help="Recorded Mneme DB files", nargs="+")
+        parser.set_defaults(func=Clean.run)
+
+    @staticmethod
+    def run(args):
+        dbs = args.dbs
+        for db in dbs:
+            if not Path(db).exists():
+                print(f"{db} does not exist")
+                sys.exit(-1)
+        ir_records = set()
+        mneme_db_files = set()
+        for db in dbs:
+            exec = RecordedExecution.from_json(db)
+            ir_records |= {Path(e) for e in exec.llvm_files}
+            for _, kernel in exec.items():
+                mneme_db_files.add(Path(kernel.prologue.fn))
+                mneme_db_files.add(Path(kernel.epilogue.fn))
+            Path(db).unlink()
+
+        for fn in ir_records:
+            if fn.exists():
+                fn.unlink()
+
+        for fn in mneme_db_files:
+            if fn.exists():
+                fn.unlink()
+
+
+class Copy:
+    @staticmethod
+    def set_cli_args(parser):
+        parser.add_argument(
+            "dbs",
+            nargs="+",
+            help="Source paths of Mneme DB records followed by a destination path to which data will be copied to",
+        )
+        parser.set_defaults(func=Copy.run)
+
+    @staticmethod
+    def run(args):
+        paths = args.dbs
+        if len(paths) < 2:
+            raise RuntimeError(
+                f"Please provide both source and destination directories {paths}"
+            )
+
+        *_sources, _dest = paths
+        dest = Path(_dest).absolute()
+        if not dest.exists():
+            raise RuntimeError("Destination directory does not exist")
+        if not dest.is_dir():
+            raise RuntimeError("Expected Destination to be a directory")
+
+        sources = [Path(s) for s in _sources]
+        _copy_or_move(sources, dest)
+
+
+class Move:
+    @staticmethod
+    def set_cli_args(parser):
+        parser.add_argument(
+            "dbs",
+            nargs="+",
+            help="Source paths of Mneme DB records followed by a destination path to which data will be moved to",
+        )
+        parser.set_defaults(func=Move.run)
+
+    @staticmethod
+    def run(args):
+        paths = args.dbs
+        if len(paths) < 2:
+            raise RuntimeError("Please provide both source and destination directories")
+
+        *_sources, _dest = paths
+        dest = Path(_dest).absolute()
+        if not dest.exists():
+            raise RuntimeError("Destination directory does not exist")
+        if not dest.is_dir():
+            raise RuntimeError("Expected Destination to be a directory")
+
+        sources = [Path(s) for s in _sources]
+        _copy_or_move(sources, dest, move=True)
