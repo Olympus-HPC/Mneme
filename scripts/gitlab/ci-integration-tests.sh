f02e42c1190c364a154ba767b1bfcfb8466b9429
Loic Pottier <pottier1@llnl.gov>
2025-10-03 14:31:10 -0700

    Added time measurements and debug print
    Signed-off-by: Loic Pottier <pottier1@llnl.gov>

--- scripts/gitlab/ci-integration-tests.sh
+++ scripts/gitlab/ci-integration-tests.sh
@@ -9,6 +9,8 @@ PR_INFO=$(curl -s -L -H "Authorization: Bearer $GITHUB_TOKEN" \
                -H "X-GitHub-Api-Version: 2022-11-28" \
                "https://api.github.com/repos/Olympus-HPC/Mneme/pulls?head=Olympus-HPC:${CI_COMMIT_REF_NAME}")
 
+echo "PR_INFO = ${PR_INFO}"
+
 # Check if PR exists.
 if [ -z "${PR_INFO}" ] || [ "$(echo "$PR_INFO" | jq length)" = "0" ]; then
   echo "No PR found for ref ${CI_COMMIT_REF_NAME}, exit"
@@ -33,5 +35,18 @@ else
   exit 0
 fi
 
-# Run the selected benchmarks
-(bash -c "${BENCHMARKS}")
+RESULTS_COMMIT="Artifacts PR ${PR_ID} commit ${CI_COMMIT_SHORT_SHA}"
+
+# Run the selected benchmark
+SECONDS=0
+bash -c "${BENCHMARKS}"
+END_TIME=$SECONDS
+COMMENT="${RESULTS_COMMIT}: ${COMMENTS_BODY} ran in ${END_TIME} seconds\n<p>"
+
+# Post the comment to the GitHub PR.
+curl -L -X POST \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer $GITHUB_TOKEN" \
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  "https://api.github.com/repos/Olympus-HPC/Mneme/issues/${PR_ID}/comments" \
+  -d "{\"body\": \"${COMMENT}\"}"
