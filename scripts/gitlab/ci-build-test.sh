f785c7d8b10cd51b2512892aa36927268fff201e
koparasy <parasyris1@llnl.gov>
2025-10-23 12:44:40 -0700

    Fixed CI to not clone spdlog when logger is off

--- scripts/gitlab/ci-build-test.sh
+++ scripts/gitlab/ci-build-test.sh
@@ -41,6 +41,10 @@ build_proteus() {
 }
 
 build_spdlog() {
+  if [[ "${MNEME_CI_ENABLE_LOGGER^^}" -eq "OFF" ]]; then
+	  return
+  fi
+
   echo "Building SPDLOG"
   git clone --depth 1 --branch v1.15.0  --single-branch https://github.com/gabime/spdlog.git
   pushd spdlog
@@ -105,7 +109,7 @@ cmake \
 -DLLVM_INSTALL_DIR=${LLVM_INSTALL_DIR} \
 -DMNEME_ENABLE_HIP=Off \
 -DMNEME_ENABLE_CUDA=On \
--DMNEME_ENABLE_DEBUG=${MNEME_CI_ENABLE_DEBUG} \
+-DMNEME_ENABLE_LOGGER=${MNEME_CI_ENABLE_LOGGER} \
 -DMNEME_ENABLE_TESTS=On \
 -DCMAKE_EXPORT_COMPILE_COMMANDS=on ${mneme_src}
 
@@ -129,7 +133,7 @@ cmake \
 -DCMAKE_C_COMPILER=amdclang \
 -DLLVM_INSTALL_DIR=${LLVM_INSTALL_DIR} \
 -DMNEME_ENABLE_HIP=On \
--DMNEME_ENABLE_DEBUG=${MNEME_CI_ENABLE_DEBUG} \
+-DMNEME_ENABLE_LOGGER=${MNEME_CI_ENABLE_LOGGER} \
 -DMNEME_ENABLE_TESTS=On \
 -DCMAKE_EXPORT_COMPILE_COMMANDS=on ${mneme_src}
 fi
