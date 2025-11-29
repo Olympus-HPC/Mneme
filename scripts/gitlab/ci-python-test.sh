67fe98b0af4532f41ad15414f64b4212929053d5
koparasy <parasyris1@llnl.gov>
2025-11-28 18:45:02 -0800

    Add coverage

--- scripts/gitlab/ci-python-test.sh
+++ scripts/gitlab/ci-python-test.sh
@@ -15,5 +15,13 @@ python -m venv venv
 source venv/bin/activate
 pip install ${mneme_src}
 pip install pytest pytest-cov
-pytest -v -s ${mneme_src}/python/tests/ 
+pytest -v -s ${mneme_src}/python/tests/ || exit $? 
+
+# Upload to Codecov (only if token is available)
+if [[ -n "$CODECOV_TOKEN" ]]; then
+    echo "Uploading coverage to Codecov..."
+    curl -s https://codecov.io/bash | bash -s -- -t "$CODECOV_TOKEN" -f coverage.xml || echo "Codecov upload failed"
+else
+    echo "No CODECOV_TOKEN set, skipping upload."
+fi
 
