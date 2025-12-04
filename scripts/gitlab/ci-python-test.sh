62b105a63a0e794e145bc66c1f4b42040459426b
Konstantinos Parasyris <parasyris1@llnl.gov>
2025-12-04 08:15:48 -0800

    Update codegov push script (#70)

--- scripts/gitlab/ci-python-test.sh
+++ scripts/gitlab/ci-python-test.sh
@@ -25,20 +25,29 @@ rm -rf ${VENV_NAME}/lib*/python*/site-packages/mneme
 python -m pip install ${mneme_src}
 python -m pip install pytest pytest-cov
 pytest -v -s ${mneme_src}/python/tests/ || exit $? 
+pushd ${mneme_src}
+pytest --cov-report=xml:coverage-${CI_JOB_ID}.xml --cov-config=.coveragerc python/tests/
 
 # Upload to Codecov (only if token is available)
 if [[ -n "$CODECOV_TOKEN" ]]; then
     echo "Uploading coverage to Codecov..."
-    curl -Os https://uploader.codecov.io/latest/linux/codecov
+    echo "SHA is $CI_COMMIT_SHA"
+    echo "Branch is $CI_COMMIT_BRANCH"
+    export NODE_TLS_REJECT_UNAUTHORIZED=0
+
+    curl -k -Os https://uploader.codecov.io/latest/linux/codecov
     chmod +x codecov
     ./codecov \
         -t "$CODECOV_TOKEN" \
-        -f coverage.xml \
+        -f coverage-${CI_JOB_ID}.xml \
         -C "$CI_COMMIT_SHA" \
         -B "$CI_COMMIT_BRANCH" \
-        -r "Olympus-HPC/Mneme" || echo "Codecov upload failed"
+        --insecure \
+        --disable-ci-detection \
+        --slug=gh/Olympus-HPC/Mneme || echo "Codecov upload failed"
 else
     echo "No CODECOV_TOKEN set, skipping upload."
 fi
 
+rm -f coverage-${CI_JOB_ID}.xml
 deactivate
