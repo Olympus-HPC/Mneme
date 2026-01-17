29b855cea309a4eec227daa6a2d1d9310f0ccb01
koparasy <parasyris1@llnl.gov>
2026-01-16 18:18:24 -0800

    Add scripts

--- scripts/gitlab/ci-python-test.sh
+++ scripts/gitlab/ci-python-test.sh
@@ -1,40 +1,46 @@
 #!/bin/bash
 
-set -e
+set -euo pipefail
+trap 'echo "[ERROR] line $LINENO" >&2' ERR
+
+log() { echo " #### [$(date +%T)] $* ###" >&2; }
+
+log "CI job $CI_JOB_ID starting"
 
 if [[ -n "$CODECOV_TOKEN" ]]; then
-  echo "CODECOV_TOKEN is set"
+  log "CODECOV_TOKEN is set"
 else
-  echo "CODECOV_TOKEN is not set"
+  log "CODECOV_TOKEN is not set"
 fi
 
-echo "CI JOB ID IS ${CI_JOB_ID}"
+log "CI JOB ID IS ${CI_JOB_ID}"
 
 test_dir="$(mktemp -d)"
 mkdir -p ${test_dir}
 
 # Make a copy of src to local FS to accelerate building etc. 
+log "Start copying src to temp"
 mneme_orig_src=$(pwd)
 mkdir -p $test_dir/mneme_src/
 rsync -a --delete "$mneme_orig_src" "$test_dir/mneme_src/"
 mneme_src=$test_dir/mneme_src/Mneme
-echo "Mneme copied src is ${mneme_src} vs ${mneme_orig_src}"
+log "End copying mneme src to ${mneme_src} from ${mneme_orig_src}"
 
 ml load python/${MNEME_CI_PYTHON_VERSION}
 ml load rocm/${MNEME_CI_ROCM_VERSION}
 export LLVM_INSTALL_DIR=${ROCM_PATH}/
-echo "Test dir is ${test_dir}"
+log "Test dir is ${test_dir}"
 
 
 # Make a copy of preinstalled deps to local FS for fast installation
 #VENV_NAME="/usr/workspace/LExperts/ci/gitlab/venv-${LCSCHEDCLUSTER}-${MNEME_CI_ROCM_VERSION}-${MNEME_CI_PYTHON_VERSION}/"
 #rm -rf ${VENV_NAME}/lib*/python*/site-packages/mneme
 
+log "Starting making environment"
 LOCAL_VENV_NAME="${test_dir}/venv/"
 mkdir -p ${LOCAL_VENV_NAME}
 python -m venv "${LOCAL_VENV_NAME}"
 #rsync -a "$VENV_NAME/" "$LOCAL_VENV_NAME"
-
 pushd ${test_dir}
 source ${LOCAL_VENV_NAME}/bin/activate
 echo "Environment is: ${LOCAL_VENV_NAME}"
@@ -42,17 +48,26 @@ echo "ENV will use python:"
 which python
 
 python -m pip uninstall -y mneme
+log "Environment made"
+
+log "Start installing mneme"
 python -m pip  -v install ${mneme_src}
+log "Finalized with installation"
+
+log "Installing pytest"
 python -m pip install pytest pytest-cov
+
+log "Start running mneme tests"
 pytest -v -s ${mneme_src}/python/tests/ || exit $? 
+log "Done with testing"
 pushd ${mneme_src}
 pytest --cov-report=xml:coverage-${CI_JOB_ID}.xml --cov-config=.coveragerc python/tests/
 
 # Upload to Codecov (only if token is available)
 if [[ -n "$CODECOV_TOKEN" ]]; then
-    echo "Uploading coverage to Codecov..."
-    echo "SHA is $CI_COMMIT_SHA"
-    echo "Branch is $CI_COMMIT_BRANCH"
+    log "Uploading coverage to Codecov..."
+    log "SHA is $CI_COMMIT_SHA"
+    log "Branch is $CI_COMMIT_BRANCH"
     export NODE_TLS_REJECT_UNAUTHORIZED=0
 
     curl -k -Os https://uploader.codecov.io/latest/linux/codecov
@@ -76,13 +91,14 @@ python -m pip uninstall -y mneme
 python -m pip install -U pip setuptools wheel
 
 # Try a editable install
-echo "Removed mneme"
-echo "Mneme src is ${mneme_src}"
+log "Removed mneme"
+log "Mneme src is ${mneme_src}"
+log "Start editable install"
 python -m pip install -e ${mneme_src}
-echo "Install mneme in dev mode"
 # If everything is properly installed this command will not fail
+log "End editable install"
+
 mneme config cxx || exit $?
-python -m pip uninstall -y mneme
 
 deactivate
 rm -rf ${test_dir}
