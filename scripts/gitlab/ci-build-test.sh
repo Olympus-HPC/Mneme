3055f7b89c03da3fef360b7b77808d2597bf7f50
koparasy <parasyris1@llnl.gov>
2026-01-19 14:36:11 -0800

    Adding matrix in the ci

--- scripts/gitlab/ci-build-test.sh
+++ scripts/gitlab/ci-build-test.sh
@@ -16,7 +16,7 @@ pushd $build_dir
 build_proteus() {
   echo "Building PROTEUS"
   if [[ ! -d proteus ]]; then
-  	git clone --depth 1 git@github.com:Olympus-HPC/proteus.git
+    git clone --depth 1 git@github.com:Olympus-HPC/proteus.git
     pushd proteus
     git fetch --depth 1 origin features/cuda-shared 
     git checkout -b features/cuda-shared FETCH_HEAD
@@ -32,19 +32,19 @@ build_proteus() {
   mkdir build-proteus-${host}
   pushd build-proteus-${host}
   cmake .. \
-  -DBUILD_SHARED=On \
-  -DCMAKE_POSITION_INDEPENDENT_CODE=On \
-  -DLLVM_INSTALL_DIR=${LLVM_INSTALL_DIR} \
-  -DCMAKE_C_COMPILER=${LLVM_INSTALL_DIR}/bin/clang \
-  -DCMAKE_PREFIX_PATH="$CONDA_PREFIX;$CONDA_PREFIX/lib/cmake" \
-  -DCMAKE_CXX_COMPILER=${LLVM_INSTALL_DIR}/bin/clang++ \
-  -DPROTEUS_ENABLE_HIP=${PROTEUS_ENABLE_HIP} \
-  -DPROTEUS_LINK_SHARED_LLVM=${LINK_SHARED_LLVM} \
-  -DPROTEUS_ENABLE_CUDA=${PROTEUS_ENABLE_CUDA} \
-  -DCMAKE_EXPORT_COMPILE_COMMANDS=on \
-  -DCMAKE_CUDA_ARCHITECTURES=90 \
-  -DENABLE_TESTS=Off \
-  -DCMAKE_INSTALL_PREFIX=${PROTEUS_INSTALL_DIR}
+    -DBUILD_SHARED=On \
+    -DCMAKE_POSITION_INDEPENDENT_CODE=On \
+    -DLLVM_INSTALL_DIR=${LLVM_INSTALL_DIR} \
+    -DCMAKE_C_COMPILER=${LLVM_INSTALL_DIR}/bin/clang \
+    -DCMAKE_PREFIX_PATH="$CONDA_PREFIX;$CONDA_PREFIX/lib/cmake" \
+    -DCMAKE_CXX_COMPILER=${LLVM_INSTALL_DIR}/bin/clang++ \
+    -DPROTEUS_ENABLE_HIP=${PROTEUS_ENABLE_HIP} \
+    -DPROTEUS_LINK_SHARED_LLVM=${LINK_SHARED_LLVM} \
+    -DPROTEUS_ENABLE_CUDA=${PROTEUS_ENABLE_CUDA} \
+    -DCMAKE_EXPORT_COMPILE_COMMANDS=on \
+    -DCMAKE_CUDA_ARCHITECTURES=90 \
+    -DENABLE_TESTS=Off \
+    -DCMAKE_INSTALL_PREFIX=${PROTEUS_INSTALL_DIR}
   make -j 10
   make install -j 10
   popd
@@ -54,13 +54,13 @@ build_proteus() {
 build_spdlog() {
   echo "Building spdlog ${MNEME_CI_ENABLE_LOGGER} ${MNEME_CI_ENABLE_LOGGER^^}"
   if [[ "${MNEME_CI_ENABLE_LOGGER^^}" == "OFF" ]]; then
-	echo "Early exit"
-	return
+    echo "Early exit"
+    return
   fi
 
   echo "Building SPDLOG"
   if [[ ! -d spdlog ]]; then
-  git clone --depth 1 --branch v1.15.0  --single-branch https://github.com/gabime/spdlog.git
+    git clone --depth 1 --branch v1.15.0  --single-branch https://github.com/gabime/spdlog.git
   fi
   pushd spdlog
   SPDLOG_INSTALL_DIR=$1
@@ -68,10 +68,10 @@ build_spdlog() {
   mkdir build-spdlog-${host}
   pushd build-spdlog-${host}
   cmake \
-  -DCMAKE_C_COMPILER=${LLVM_INSTALL_DIR}/bin/clang \
-  -DCMAKE_CXX_COMPILER=${LLVM_INSTALL_DIR}/bin/clang++ \
-  -DCMAKE_INSTALL_PREFIX=${SPDLOG_INSTALL_DIR} \
-  ..
+    -DCMAKE_C_COMPILER=${LLVM_INSTALL_DIR}/bin/clang \
+    -DCMAKE_CXX_COMPILER=${LLVM_INSTALL_DIR}/bin/clang++ \
+    -DCMAKE_INSTALL_PREFIX=${SPDLOG_INSTALL_DIR} \
+    ..
 
   make -j 10
   make install -j 10
@@ -80,25 +80,25 @@ build_spdlog() {
 }
 
 if [[ "$SYS_TYPE" == "toss_4_x86_64_ib" ]]; then
-ml load cmake/3.30
-ml load cuda/12.2.2
-PYTHON_VERSION=3.12
+  ml load cmake/3.30
+  ml load cuda/12.2.2
+  PYTHON_VERSION=3.12
 
 # Install Clang/LLVM through conda.
 MINICONDA_DIR=miniconda3
 if [[ ! -d ${MINICONDA_DIR} ]]; then
-mkdir -p ${MINICONDA_DIR}
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-$(uname -m).sh -O ./${MINICONDA_DIR}/miniconda.sh
-bash ./${MINICONDA_DIR}/miniconda.sh -b -u -p ./${MINICONDA_DIR}
-rm ./${MINICONDA_DIR}/miniconda.sh
-source "${MINICONDA_DIR}/etc/profile.d/conda.sh"
-conda activate base
-conda create -y -n mneme -c conda-forge \
+  mkdir -p ${MINICONDA_DIR}
+  wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-$(uname -m).sh -O ./${MINICONDA_DIR}/miniconda.sh
+  bash ./${MINICONDA_DIR}/miniconda.sh -b -u -p ./${MINICONDA_DIR}
+  rm ./${MINICONDA_DIR}/miniconda.sh
+  source "${MINICONDA_DIR}/etc/profile.d/conda.sh"
+  conda activate base
+  conda create -y -n mneme -c conda-forge \
     python=${PYTHON_VERSION} clang=${MNEME_CI_LLVM_VERSION} clangxx=${MNEME_CI_LLVM_VERSION} \
-      clangdev=${MNEME_CI_LLVM_VERSION} llvmdev=${MNEME_CI_LLVM_VERSION} lit=${MNEME_CI_LLVM_VERSION} \
-      gcc=12 gxx=12
+    clangdev=${MNEME_CI_LLVM_VERSION} llvmdev=${MNEME_CI_LLVM_VERSION} lit=${MNEME_CI_LLVM_VERSION} \
+    gcc=12 gxx=12
 else
-source "${MINICONDA_DIR}/etc/profile.d/conda.sh"
+  source "${MINICONDA_DIR}/etc/profile.d/conda.sh"
 fi
 conda activate mneme
 
@@ -113,46 +113,46 @@ echo "After proteus Current directory is $(pwd)"
 build_spdlog $installDir
 echo "Current dir is $(pwd)"
 cmake \
--DCMAKE_BUILD_TYPE=Debug \
--DCMAKE_CUDA_FLAGS=-std=c++17 \
--DCMAKE_PREFIX_PATH="$CONDA_PREFIX;$CONDA_PREFIX/lib/cmake" \
--Dproteus_DIR=$installDir \
--DCMAKE_INSTALL_PREFIX=$installDir \
--DCMAKE_CXX_COMPILER=${cpp} \
--DCMAKE_C_COMPILER=${cc} \
--DMNEME_LINK_SHARED_LLVM=ON \
--DCMAKE_CUDA_COMPILER=${cpp} \
--DCMAKE_CUDA_ARCHITECTURES=90 \
--DLLVM_INSTALL_DIR=${LLVM_INSTALL_DIR} \
--DMNEME_ENABLE_HIP=Off \
--DMNEME_ENABLE_CUDA=On \
--DMNEME_ENABLE_LOGGER=${MNEME_CI_ENABLE_LOGGER} \
--DMNEME_ENABLE_TESTS=On \
--DCMAKE_EXPORT_COMPILE_COMMANDS=on ${mneme_src}
+  -DCMAKE_BUILD_TYPE=Debug \
+  -DCMAKE_CUDA_FLAGS=-std=c++17 \
+  -DCMAKE_PREFIX_PATH="$CONDA_PREFIX;$CONDA_PREFIX/lib/cmake" \
+  -Dproteus_DIR=$installDir \
+  -DCMAKE_INSTALL_PREFIX=$installDir \
+  -DCMAKE_CXX_COMPILER=${cpp} \
+  -DCMAKE_C_COMPILER=${cc} \
+  -DMNEME_LINK_SHARED_LLVM=ON \
+  -DCMAKE_CUDA_COMPILER=${cpp} \
+  -DCMAKE_CUDA_ARCHITECTURES=90 \
+  -DLLVM_INSTALL_DIR=${LLVM_INSTALL_DIR} \
+  -DMNEME_ENABLE_HIP=Off \
+  -DMNEME_ENABLE_CUDA=On \
+  -DMNEME_ENABLE_LOGGER=${MNEME_CI_ENABLE_LOGGER} \
+  -DMNEME_ENABLE_TESTS=On \
+  -DCMAKE_EXPORT_COMPILE_COMMANDS=on ${mneme_src}
 
 
 elif [[ "$SYS_TYPE" == "toss_4_x86_64_ib_cray" ]]; then
-ml load rocm/${MNEME_CI_ROCM_VERSION}
-export LLVM_INSTALL_DIR=${ROCM_PATH}/llvm
-echo "LLVM INSTALL DIR is ${LLVM_INSTALL_DIR}"
+  ml load rocm/${MNEME_CI_ROCM_VERSION}
+  export LLVM_INSTALL_DIR=${ROCM_PATH}/llvm
+  echo "LLVM INSTALL DIR is ${LLVM_INSTALL_DIR}"
 
-build_proteus "ON" "OFF" $installDir OFF
-echo "After proteus Current directory is $(pwd)"
-build_spdlog $installDir
-echo "After spdlog Current directory is $(pwd)"
+  build_proteus "ON" "OFF" $installDir OFF
+  echo "After proteus Current directory is $(pwd)"
+  build_spdlog $installDir
+  echo "After spdlog Current directory is $(pwd)"
 
 
-cmake \
--DCMAKE_BUILD_TYPE=Relwithdebinfo \
--Dproteus_DIR=$installDir \
--DCMAKE_INSTALL_PREFIX=$installDir \
--DCMAKE_CXX_COMPILER=amdclang++ \
--DCMAKE_C_COMPILER=amdclang \
--DLLVM_INSTALL_DIR=${LLVM_INSTALL_DIR} \
--DMNEME_ENABLE_HIP=On \
--DMNEME_ENABLE_LOGGER=${MNEME_CI_ENABLE_LOGGER} \
--DMNEME_ENABLE_TESTS=On \
--DCMAKE_EXPORT_COMPILE_COMMANDS=on ${mneme_src}
+  cmake \
+    -DCMAKE_BUILD_TYPE=Relwithdebinfo \
+    -Dproteus_DIR=$installDir \
+    -DCMAKE_INSTALL_PREFIX=$installDir \
+    -DCMAKE_CXX_COMPILER=amdclang++ \
+    -DCMAKE_C_COMPILER=amdclang \
+    -DLLVM_INSTALL_DIR=${LLVM_INSTALL_DIR} \
+    -DMNEME_ENABLE_HIP=On \
+    -DMNEME_ENABLE_LOGGER=${MNEME_CI_ENABLE_LOGGER} \
+    -DMNEME_ENABLE_TESTS=On \
+    -DCMAKE_EXPORT_COMPILE_COMMANDS=on ${mneme_src}
 fi
 
 
