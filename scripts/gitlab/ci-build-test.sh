7fddd3988b0fb0ef8908f0a8b51f60e6e09df420
koparasy <parasyris1@llnl.gov>
2026-01-21 08:25:42 -0800

    Fixes on CI

--- scripts/gitlab/ci-build-test.sh
+++ scripts/gitlab/ci-build-test.sh
@@ -5,7 +5,7 @@ set -e
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 
-if [[ -f "$SCRIPT_DIR/mneme-common-ci.sh" ]] then
+if [[ -f "$SCRIPT_DIR/mneme-common-ci.sh" ]]; then
   source "$SCRIPT_DIR/mneme-common-ci.sh"
 else
   SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
--- scripts/gitlab/ci-python-test.sh
+++ scripts/gitlab/ci-python-test.sh
@@ -4,7 +4,7 @@ set -ex
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-if [[ -f "$SCRIPT_DIR/mneme-common-ci.sh" ]] then
+if [[ -f "$SCRIPT_DIR/mneme-common-ci.sh" ]]; then
   source "$SCRIPT_DIR/mneme-common-ci.sh"
 else
   SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
